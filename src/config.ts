5c7c012d1e7e4dc001d1f37fb65eb4cf87616f06
Iurii <37527853+testmetonplease@users.noreply.github.com>
2019-02-18 19:56:49 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,4 @@
 // Initialize Firebase
 export const firebaseConfig = {
-  // apiKey: 'AIzaSyDkqe1nrYqbzqEpCwnKmhRMTd-1nLqQyOY',
-  // authDomain: 'phonecard-d105b.firebaseapp.com',
-  databaseURL: ' https://hacker-news.firebaseio.com'
-  // projectId: 'phonecard-d105b',
-  // storageBucket: 'phonecard-d105b.appspot.com',
-  // messagingSenderId: '232484998060'
+   databaseURL: ' https://hacker-news.firebaseio.com'
 };
